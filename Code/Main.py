0b14203e18ab597f3f6b8c903c750d21254fc94a
Tanay Jain <tanayjjj@gmail.com>
2022-06-24 22:10:47 -0700

    Update Main.py
    data ingest

--- Code/Main.py
+++ Code/Main.py
@@ -5,3 +5,16 @@ Created on Fri Jun 24 21:16:29 2022
 @author: tanay
 """
 
+#DATA Ingest from local drive
+
+def data_ingest(dir):
+    
+    from os import chdir
+    chdir(dir)
+
+    import pandas as pd
+    
+    return(pd.read_csv("train.csv"), pd.read_csv("test.csv"))
+
+
+train, test = data_ingest("C:\\Users\\tanay\\Documents\\GitHub\\House-Prices-Advanced-Regression-Techniques\\Data")
